7f77d06fbb82a5006d60f80fd49fd17b2f58cf18
ndungu-kr <85244705+ndungu-kr@users.noreply.github.com>
2026-03-15 16:41:36 +0000

    added lcd functionality to arduino

--- temp_warning_system/temp_warning_system.ino
+++ temp_warning_system/temp_warning_system.ino
@@ -8,6 +8,8 @@
 #include <DHT.h>
 // Include the SoftwareSerial library so we can use digital pins for Bluetooth.
 #include <SoftwareSerial.h>
+// Include Grove LCD library 
+#include <rgb_lcd.h>
 
 #define DHT_PIN 4
 #define DHT_TYPE DHT11
@@ -18,6 +20,8 @@
 // Create a DHT object using the pin and type we defined
 DHT dht(DHT_PIN, DHT_TYPE);
 
+// Create an LCD object
+rgb_lcd lcd;
 
 SoftwareSerial blueToothSerial(RxD, TxD);
 
@@ -26,6 +30,9 @@ void setup()
   blueToothSerial.begin(9600);
   dht.begin();
 
+  // Start the LCD with 16 columns and 2 rows
+  lcd.begin(16, 2);
+
   pinMode(BUZZER_PIN, OUTPUT);
 }
 
@@ -33,6 +40,25 @@ void loop()
 {
   float temperature = dht.readTemperature();
   float humidity = dht.readHumidity();
+
+  // Clear the LCD screen ready for new values
+  lcd.clear();
+
+  lcd.setCursor(0, 0);
+  lcd.print("Temp: ");
+  lcd.print(temperature);
+  lcd.print("C");
+
+  lcd.setCursor(0, 1);
+  lcd.print("Hum: ");
+  lcd.print(humidity);
+  lcd.print("%");
+
+  if (temperature > 25) {
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("HIGH TEMP!");
+  }
 
   if (humidity >= 80) {
     digitalWrite(BUZZER_PIN, HIGH);
